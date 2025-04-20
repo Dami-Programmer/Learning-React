68f672baedffb50d4e35ce26472f6fca26ce0428
Damilola <Damilola2554@gmail.com>
2025-04-20 19:11:23 +0100

    learnt about condition Template and did a little on show even list

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,26 +2,44 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("mairo");
+  const [showEvents, setShowEvents] = useState(true);
   const [events, setEvents] = useState([
     { title: "mario's birthday bash", id: 1 },
     { title: "bowser's live stream", id: 2 },
     { title: "race on moo moo farm", id: 3 },
   ]);
 
-  const handleClick = () => {
-    setName("Dami");
-    console.log(name);
+  console.log(showEvents);
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
   };
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}> Change name </button>
-      {events.map((event) => (
-        <div key={event.id}>
-          <h2>{event.title}</h2>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
         </div>
-      ))}
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
     </div>
   );
 }
