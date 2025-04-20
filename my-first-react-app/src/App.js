45532daec3e389a304b0437002b9c1ef32fbf978
Damilola <Damilola2554@gmail.com>
2025-04-20 22:18:03 +0100

    Learn about React Fragment and the 2 types

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import Title from "./components/title";
 
 function App() {
@@ -40,12 +40,12 @@ function App() {
       )}
       {showEvents &&
         events.map((event, index) => (
-          <div key={event.id}>
+          <React.Fragment key={event.id}>
             <h2>
               {index} - {event.title}
             </h2>
             <button onClick={() => handleClick(event.id)}>delete event</button>
-          </div>
+          </React.Fragment>
         ))}
     </div>
   );
