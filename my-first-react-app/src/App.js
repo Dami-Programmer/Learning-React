441aa304d769f401de0f4a8d629b9d7371929327
Damilola <Damilola2554@gmail.com>
2025-04-21 12:13:46 +0100

    learn about Modal and how to use Props children

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import Title from "./components/title";
+import Modal from "./components/Modal";
 
 function App() {
   const [showEvents, setShowEvents] = useState(true);
@@ -47,6 +48,21 @@ function App() {
             <button onClick={() => handleClick(event.id)}>delete event</button>
           </React.Fragment>
         ))}
+
+      {/* <Modal>
+        <h2>10% off Coupon Code!!</h2>
+        <p>Use the code NINJA10 at the checkout.</p>
+      </Modal> */}
+
+      <Modal>
+        <h2>Terms and Conditions</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown
+        </p>
+        <a href="#">find out more...</a>
+      </Modal>
     </div>
   );
 }
