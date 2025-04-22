6b2dff64777bd45b5173be2a7a3275b8956071fd
Damilola <Damilola2554@gmail.com>
2025-04-22 19:11:48 +0100

    Learnt about how to use onChange state to track

--- my-first-react-app/src/components/NewEventForm.js
+++ my-first-react-app/src/components/NewEventForm.js
@@ -1,20 +1,26 @@
 import "./NewEventForm.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function NewEventForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
   return (
     <div>
       <form className="new-events-form">
         <label>
           <span>Event Title:</span>
-          <input type="text" />
+          <input type="text" onChange={(e) => setTitle(e.target.value)} />
         </label>
         <label>
           <span>Event Date:</span>
-          <input type="date" />
+          <input type="date" onChange={(e) => setDate(e.target.value)} />
         </label>
         <button>Submit</button>
+        <p>
+          title - {title}, date - {date}
+        </p>
       </form>
     </div>
   );
